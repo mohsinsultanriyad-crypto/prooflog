f9b1ed9f1c2cba578246dfb71746159207ca5253
mohsinsultanriyad-crypto <mohsinsultanriyad@gmail.com>
2026-01-15 17:05:14 +0300

    Update geo.js

--- backend/src/utils/geo.js
+++ backend/src/utils/geo.js
@@ -1,15 +1,13 @@
-const { getDistance } = require("geolib");
+function toRad(x) { return (x * Math.PI) / 180; }
 
-function metersBetween(aLat, aLng, bLat, bLng) {
-  return getDistance(
-    { latitude: aLat, longitude: aLng },
-    { latitude: bLat, longitude: bLng }
-  );
+function haversineMeters(lat1, lng1, lat2, lng2) {
+  const R = 6371000;
+  const dLat = toRad(lat2 - lat1);
+  const dLng = toRad(lng2 - lng1);
+  const a =
+    Math.sin(dLat / 2) ** 2 +
+    Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) * Math.sin(dLng / 2) ** 2;
+  return 2 * R * Math.asin(Math.sqrt(a));
 }
 
-function isInsideFence(site, lat, lng) {
-  const d = metersBetween(site.lat, site.lng, lat, lng);
-  return { ok: d <= site.radiusMeters, distanceMeters: d };
-}
-
-module.exports = { metersBetween, isInsideFence };
+module.exports = { haversineMeters };
